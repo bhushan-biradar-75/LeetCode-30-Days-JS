a41b1ce9cee18fa3ea1d805025ffc528032c2278
bhushan-biradar-75 <bhushanbiradar75@gmail.com>
2023-11-04 14:18:18 +0530

    Leetcode Cgl No 6 (2634) problem solved

---
+++ Cgl-Six.js
@@ -0,0 +1,66 @@
+// 2634 Filter Element from Array
+// Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
+
+// The fn function takes one or two arguments:
+
+// arr[i] - number from the arr
+// i - index of arr[i]
+// filteredArr should only contain the elements from the arr for which the expression fn(arr[i], i) evaluates to a truthy value. A truthy value is a value where Boolean(value) returns true.
+
+// Please solve it without the built-in Array.filter method.
+
+ 
+
+// Example 1:
+
+// Input: arr = [0,10,20,30], fn = function greaterThan10(n) { return n > 10; }
+// Output: [20,30]
+// 2636 Filter Element from Array
+
+// Explanation:
+// const newArray = filter(arr, fn); // [20, 30]
+// The function filters out values that are not greater than 10
+// Example 2:
+
+// Input: arr = [1,2,3], fn = function firstIndex(n, i) { return i === 0; }
+// Output: [1]
+// Explanation:
+// fn can also accept the index of each element
+// In this case, the function removes elements not at index 0
+// Example 3:
+
+// Input: arr = [-2,-1,0,1,2], fn = function plusOne(n) { return n + 1 }
+// Output: [-2,0,1,2]
+// Explanation:
+// Falsey values such as 0 should be filtered out
+ 
+
+// Constraints:
+
+// 0 <= arr.length <= 1000
+// -109 <= arr[i] <= 109
+
+
+const filterValue = (item) => {
+    const filterValue = item.filter((ele) => ele > 10)
+    const filterIndex = item.filter((ele , i) => i == 1)
+    const Input = item.filter((ele) => ele) //Filter method return only truthy value
+    return {filterValue, filterIndex, Input};
+}
+let item = [0,10,20,30]
+console.log(filterValue(item))//
+// Output
+// {
+//     "filterValue": [
+//         20,
+//         30
+//     ],
+//     "filterIndex": [
+//         10
+//     ],
+//     "Input": [
+//         10,
+//         20,
+//         30
+//     ]
+// }
